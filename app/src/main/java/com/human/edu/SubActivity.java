c4606fc9efb1aec4fd34797a0f4157bb50fe69a2
boramcom@daum.net <boramcom@daum.net>
2021-04-14 12:52:45 +0900

    4교시 커밋

--- app/src/main/java/com/human/edu/SubActivity.java
+++ app/src/main/java/com/human/edu/SubActivity.java
@@ -4,8 +4,19 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class SubActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
+/**
+ * 이 액티비티에서는 리사이클러뷰에 RestAPI Json 데이터를 바인딩 시키는 기능
+ * List객체(Json데이터바인딩)-어댑터클래스(데이터와 뷰객체중간)-리사이클러뷰
+ */
+public class SubActivity extends AppCompatActivity {
+    //리사이클러 뷰를 사용할 멤버변수(필드변수) 생성
+    private RecyclerAdapter mRecyclerAdapter;
+    private List mItemList = new ArrayList<MemberVO>();
+    //어댑터에서 선택한 값 확인 변수(선택한 회원을 삭제하기 위해서)
+    private int currentCursorId = -1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
