06325e5d9bd0eff9d7b3a4d61bfc6afc259c54f0
boramcom@daum.net <boramcom@daum.net>
2021-04-14 09:50:50 +0900

    1교시 커밋

--- app/src/main/java/core/PostResponseAsyncTask.java
+++ app/src/main/java/core/PostResponseAsyncTask.java
@@ -4,11 +4,17 @@ import android.app.ProgressDialog;
 import android.content.Context;
 import android.os.AsyncTask;
 
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLEncoder;
 import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 이 클래스는 AsyncTask 클래스를 상속받아서 비동기 통신(백그라운드작업) 기능 구현합니다.
@@ -82,13 +88,48 @@ public class PostResponseAsyncTask extends AsyncTask<String, Void, String> {
         try {
             url = new URL(requestUrl);
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(15000);//15초 동안 커넥션 시도
+            conn.setConnectTimeout(15000);//15초 동안만 커넥션 시도 후 중지
+            conn.setRequestMethod("POST");//스프링앱과 통신시 POST방식만 사용
+            conn.setDoInput(true);//커넥션 후 Input 허용
+            conn.setDoOutput(true);//커넥션 후 Output 허용
+            // postParms를 받아서 처리
+            OutputStream os = conn.getOutputStream();
+            BufferedWriter writer = new BufferedWriter(
+                    new OutputStreamWriter(os, "UTF-8"));
+            writer.write(getPostDataString(postDataParams)
+            );
+            writer.flush();//버퍼드 갱신=내보냅니다.
+            writer.close();//객체 소멸
+            os.close();//객체 소멸
+            int responseCode = conn.getResponseCode();//200, 204, 400
+            if(responseCode == HttpURLConnection.HTTP_OK) {
 
+            }else{
+
+            }
         } catch (MalformedURLException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
         return response;
+    }
+
+    private String getPostDataString(HashMap<String, String> postDataParams) throws UnsupportedEncodingException {
+        StringBuilder result = new StringBuilder();
+        boolean first = true; //플래그변수 
+        for(Map.Entry<String, String> entry: postDataParams.entrySet()) {
+            if(first) {
+                first = false;
+            }else{
+                result.append("&");//txtUsername=값&txtPassword=값
+            }
+            result.append(URLEncoder.encode(entry.getKey(),"UTF-8"));
+            result.append("=");
+            result.append(URLEncoder.encode(entry.getValue(),"UTF-8"));
+        }//반복문을 거치면 txtUsername=값&txtPassword=값
+        return result.toString();//형변환해서 반환
     }
 
     @Override
